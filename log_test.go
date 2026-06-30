d4560dffbbef3be5135caa011c4befc37e2bb0b3
ayuxsec <249073442+ayuxsec@users.noreply.github.com>
2026-06-30 16:42:31 +0000

    piplabu

--- log_test.go
+++ log_test.go
@@ -10,9 +10,15 @@ import (
 func TestLogger(t *testing.T) {
 	log.WithTimeStamp = false
 	log.WithTrace = true
+	log.DisableDebug = true
 	log.Info("this is an info message")
 	log.Warn("warning")
 	log.Warnf("Warning the kid just farted: %d times", 2)
+
+	log.DisableDebug = false
+	log.Debug("the kid farted 2 times but the smell of the fart was good")
+	log.DisableDebug = true
+
 	log.Errorf("error: %v", errors.New("test error"))
 	log.Fatalf("error: %v", errors.New("fatal error"))
 }
